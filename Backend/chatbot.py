386fac46d8b8ea4af681c94c9272fc923fe86963
Songquing Ye <songye@Songquings-MacBook-Air-278.local>
2026-07-21 14:01:00 -0400

    Updated app

--- Backend/chatbot.py
+++ Backend/chatbot.py
@@ -13,7 +13,7 @@ SYSTEM_INSTRUCTION = "HI"
 def get_chatbot_response(student_message, emotion=None):
     prompt = student_message
     if emotion:
-        prompt = "Here is the student message: " + " Here is what the student seems to be feeling based on their facial expressions and audio recording" + emotion
+        prompt = "Here is the student message: " + student_message + " Here is what the student seems to be feeling based on their facial expressions and audio recording" + emotion
     
     chat = client.chats.create(
         model="gemini-2.5-flash",
